1eacbc3b51ab178bf72211423e9ecf9bd926d22d
Sprajkumar <sprajkumar9755@gmail.com>
2018-05-18 22:42:28 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
-
 using namespace std;
-
-/**
-*@func GCD() returns the greatest common divisors of two integers. It accepts two arguments
-@param a : The first integer
-@param b : The second integer
-*/
-
-int GCD(int a, int b) {
-  if(a == 0)
-    return b;
-  else
-    return (b%a,a);
-}
-
 int main(int argc, char **argv) 
 {
+  int m,n;
+  cout<<"Enter the Order of matrix ";
+  cin>>m>>n;
+  int mat[m][n];
+  for(int i=0;i<m;i++)
+    for(int j=0;j<n;j++)
+      cin>>mat[i][j];
+  for(int i=0;i<m;i++)
+    for(int j=0;j<n;j++)
+      cout<<mat[i][j];
   return 0;
 }
